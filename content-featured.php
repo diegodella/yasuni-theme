c2547f52be7ab1477999a40b87ff8a604b985854
Matías Iturburu <maturburu@gmail.com>
2012-01-26 17:39:12 -0300

    Removidos slots innecesarios

--- content-featured.php
+++ content-featured.php
@@ -23,25 +23,4 @@ global $feature_class;
 		<?php wp_link_pages( array( 'before' => '<div class="page-link"><span>' . __( 'Pages:', 'yasuni' ) . '</span>', 'after' => '</div>' ) ); ?>
 	</div><!-- .entry-content -->
 
-	<footer class="entry-meta">
-		<?php
-			/* translators: used between list items, there is a space after the comma */
-			$tag_list = get_the_tag_list( '', __( ', ', 'yasuni' ) );
-			if ( '' != $tag_list ) {
-				$utility_text = __( 'This entry was posted in %1$s and tagged %2$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'yasuni' );
-			} else {
-				$utility_text = __( 'This entry was posted in %1$s. Bookmark the <a href="%3$s" title="Permalink to %4$s" rel="bookmark">permalink</a>.', 'yasuni' );
-			}
-			printf(
-				$utility_text,
-				/* translators: used between list items, there is a space after the comma */
-				get_the_category_list( __( ', ', 'yasuni' ) ),
-				$tag_list,
-				esc_url( get_permalink() ),
-				the_title_attribute( 'echo=0' )
-			);
-		?>
-
-		<?php edit_post_link( __( 'Edit', 'yasuni' ), '<span class="edit-link">', '</span>' ); ?>
-	</footer><!-- .entry-meta -->
 </article><!-- #post-<?php the_ID(); ?> -->
