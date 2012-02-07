cd6905b5b5ec791bfbdc9aebde63fbc0cad874b3
Matías Iturburu <maturburu@gmail.com>
2012-02-07 16:50:17 -0300

    Retoques sobre lo que hizo matiass

--- header.php
+++ header.php
@@ -89,7 +89,7 @@
 	    <?php dynamic_sidebar( 'sidebar-6' ); ?>
 	    <?php endif; ?>
 
-		<div id="parallax">
+		<div id="parallax" <?php if ( !is_front_page() ) { echo 'class="inner"';};?>>
             <div id="header_image" class = "<?php if ( is_front_page() ) echo "home"; ?>">
                 <a href="<?php echo esc_url( home_url( '/' ) ); ?>"><span>
                 <?php
@@ -107,17 +107,15 @@
                 </span></a>
             </div>
             <img id="swash" src="<?php echo get_template_directory_uri(); ?>/images/swash.png" />
+        	<img id="asset_tree_a"  src="<?php echo get_template_directory_uri(); ?>/images/asset_tree.png" />
+        	<img id="asset_tree_1" src="<?php echo get_template_directory_uri(); ?>/images/asset_tree_1.png" />
+        	<img id="asset_bird_1" src="<?php echo get_template_directory_uri(); ?>/images/asset_bird_1.png" />
             <?php if ( is_front_page() ) { ?>
-            	<img id="asset_tree_a"  src="<?php echo get_template_directory_uri(); ?>/images/asset_tree.png" />
-            	<img id="asset_tree_1" src="<?php echo get_template_directory_uri(); ?>/images/asset_tree_1.png" />
-            	<img id="asset_bird" src="<?php echo get_template_directory_uri(); ?>/images/asset_bird.png" />
-            	<img id="asset_bird_1" src="<?php echo get_template_directory_uri(); ?>/images/asset_bird_1.png" />
-            	<img id="swash1" src="<?php echo get_template_directory_uri(); ?>/images/swash_1.png" />
-            	<img id="slogan" src="<?php echo get_template_directory_uri(); ?>/images/slogan.png" />
+        	<img id="asset_bird" src="<?php echo get_template_directory_uri(); ?>/images/asset_bird.png" />
+
+        	<img id="swash1" src="<?php echo get_template_directory_uri(); ?>/images/swash_1.png" />
+        	<img id="slogan" src="<?php echo get_template_directory_uri(); ?>/images/slogan.png" />
 			<?php } else { ?>
-				<img id="asset_tree_b"  src="<?php echo get_template_directory_uri(); ?>/images/asset_tree.png" />
-				<img id="asset_tree_2" src="<?php echo get_template_directory_uri(); ?>/images/asset_tree_1.png" />
-				<img id="asset_bird_2" src="<?php echo get_template_directory_uri(); ?>/images/asset_bird_1.png" />
 				<img id="swash2" src="<?php echo get_template_directory_uri(); ?>/images/swash_1.png" />
 				<img id="asset_bird_blue" src="<?php echo get_template_directory_uri(); ?>/images/asset_bird_blue.png" />
 			<?php } ?>
