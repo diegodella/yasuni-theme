ed2eb601d3d79313cece4ec5a82acc6a3a7c5e89
Matías Iturburu <maturburu@gmail.com>
2012-02-14 02:29:21 -0300

    Cambios menores

--- sidebar.php
+++ sidebar.php
@@ -13,6 +13,21 @@ $current_layout = $options['theme_layout'];
 if ( 'content' != $current_layout ) :
 ?>
 		<div id="secondary" class="widget-area" role="complementary">
+            <aside id="latest-videos" class="column center">
+                <h3>Videos</h3>
+                <?php
+                query_posts( 'category_name=Videos&showposts=1' );
+                while (have_posts() ) : the_post();
+                ?>
+                <article>
+                    <?php the_content(); ?>
+                </article>
+                <?php
+                endwhile;
+                wp_reset_query();
+                ?>
+            </aside>
+
 			<?php if ( ! dynamic_sidebar( 'sidebar-1' ) ) : ?>
 
 				<aside id="archives" class="widget">
@@ -21,16 +36,6 @@ if ( 'content' != $current_layout ) :
 						<?php wp_get_archives( array( 'type' => 'monthly' ) ); ?>
 					</ul>
 				</aside>
-
-				<aside id="meta" class="widget">
-					<h3 class="widget-title"><?php _e( 'Meta', 'yasuni' ); ?></h3>
-					<ul>
-						<?php wp_register(); ?>
-						<li><?php wp_loginout(); ?></li>
-						<?php wp_meta(); ?>
-					</ul>
-				</aside>
-
 			<?php endif; // end sidebar widget area ?>
 		</div><!-- #secondary .widget-area -->
 <?php endif; ?>
