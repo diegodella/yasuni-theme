8ac2df1c023eb1292b3f17d852a187b8eb7a6e90
Matías Iturburu <maturburu@gmail.com>
2012-02-28 18:55:38 -0300

    logos nuevos

--- header.php
+++ header.php
@@ -42,17 +42,21 @@
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
 <link rel="shortcut icon" href="<?php bloginfo('template_directory')?>/images/favicon.ico" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
+
 <script src="<?php echo get_template_directory_uri(); ?>/js/jquery.js" type="text/javascript"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/js/nivo.slider.js" type="text/javascript"></script>
 <script src="<?php echo get_template_directory_uri(); ?>/js/jquery-ui.js" type="text/javascript"></script>
-<script src="<?php echo get_template_directory_uri(); ?>/js/scripts.js" type="text/javascript"></script>
+
 <!--[if lt IE 9]>
 <script src="<?php echo get_template_directory_uri(); ?>/js/html5.js" type="text/javascript"></script>
 <![endif]-->
 	<script>
 		$(function(){
 		    $('.header-slider').nivoSlider({
-	    			<?php echo boom_nivo_slider_options(); ?>
+		        effect: 'fade'
+                directionNav: false,
+                directionNavHide: false,
+                controlNav: false,
 		    });
 		})
 	</script>
